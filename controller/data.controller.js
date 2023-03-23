9d106a0fbd0ffd2b4dc5644f1d6ab2cfec00d7fb
utsav-07 <utsavudit07@gmail.com>
2023-03-23 20:40:57 +0530

    new

--- controller/data.controller.js
+++ controller/data.controller.js
@@ -19,26 +19,26 @@ const getJobData = async (req, res) => {
 
     console.log(req.query);
     console.log('jdsf');
-    // try {
+    try {
 
-    //     const options = {
-    //         method: 'GET',
-    //         url: 'https://jsearch.p.rapidapi.com/search',
-    //         params: { query: 'Python developer in Texas, USA', page: '1', num_pages: '1' },
-    //         headers: {
-    //             'X-RapidAPI-Key': 'd13f52e4efmsh934a2aa599766fbp1c53f9jsnbcfd8578ba8f',
-    //             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-    //         }
-    //     };
+        const options = {
+            method: 'GET',
+            url: `https://jsearch.p.rapidapi.com/${req.query.endpoint}`,
+            params: { query: req.query.query, num_pages: req.query.num_pages},
+            headers: {
+                'X-RapidAPI-Key': 'd13f52e4efmsh934a2aa599766fbp1c53f9jsnbcfd8578ba8f',
+                'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+            }
+        };
 
-    //     const response  = await axios.request(options);
-    //     // console.log(response.data.data);
-    //     res.status(200).json(response.data.data);
+        const response  = await axios.request(options);
+        // console.log(response.data.data);
+        res.status(200).json(response);
 
-    // } catch (error) {
-    //     console.log(error);
-    //     res.status(500).json({ message: error.messgae });
-    // }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.messgae });
+    }
 }
 
 export { getAllData, getJobData };
